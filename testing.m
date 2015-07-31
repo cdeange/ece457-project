619a3f6715d8798095d09b6cb5bc03ea6aaf8aa8
cdeange <christiandeange@gmail.com>
2015-07-31 03:30:12 -0400

    Add Bat Algorithm (bonus)

--- testing.m
+++ testing.m
@@ -30,7 +30,10 @@ addpath(strcat(pwd, '/SA'));
 addpath(strcat(pwd, '/GA'));
 addpath(strcat(pwd, '/ACO'));
 addpath(strcat(pwd, '/PSO'));
+addpath(strcat(pwd, '/BA'));
 
-[ bestFitness bestSolution fitnesses solutions ] = TabuSearch(schedule, rooms, 10, students, 10)
+% [ bestFitness bestSolution fitnesses solutions ] = TabuSearch(schedule, rooms, 3, students, 20)
 % [ bestFitness bestSolution fitnesses solutions ] = SimulatedAnnealing(schedule, rooms, students)
 % [ bestFitness bestSolution fitnesses solutions ] = Genetic(courses, students, rooms, days, timeslots);
+
+[ bestFitness bestSolution ] = BatAlgorithm(days, timeslots, courses, rooms, students)
