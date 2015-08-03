9d4c28614c491de9381f5d510c5f6703cd70d539
cdeange <christiandeange@gmail.com>
2015-08-03 16:20:40 -0400

    Remove testing files, format schedule changes

--- PrintCoursesRooms.m
+++ PrintCoursesRooms.m
@@ -6,18 +6,18 @@ function PrintCoursesRooms( schedule, students )
 
 coursemappings = [schedule.courseMappings];
 
-courseEnrollments = zeros(length(coursemappings),1);
+courseEnrollments = zeros(length(coursemappings), 1);
 
-for i=1:length(students)
+for i = 1:length(students),
     stud = students(i);
     coursesTaken = stud.enrolledCourses;
     
-    for j=1:length(coursesTaken)
+    for j = 1:length(coursesTaken)
         courseEnrollments(coursesTaken(j).courseID) = courseEnrollments(coursesTaken(j).courseID) + 1;
     end
 end
 
-for i = 1:length(coursemappings)
+for i = 1:length(coursemappings),
     course = coursemappings(i).course;
     room = coursemappings(i).room;
     
@@ -35,7 +35,7 @@ for i = 1:length(coursemappings)
     outputstring1 = sprintf ('C:%2d En:%2d Rq:%10s', courseID, courseEnrollment, courseReqStr);
     outputstring2 = sprintf ('R:%2d Cp:%2d Ft:%10s', roomID, roomCapacity, roomFeaturesStr);
     
-    fprintf('%s\n%s\n\n',outputstring1,outputstring2);
+    fprintf('%s\n%s\n\n', outputstring1, outputstring2);
     
 end
 
@@ -43,10 +43,10 @@ end
 
 function [featuresreqs] = getStringFeatures( features )
 featuresreqs = '';
-if isempty(features) ~= 1
+if isempty(features) ~= 1,
     features = sort(features);
     featuresreqs = sprintf('%d', features(1));
-    for i = 2:length(features)
+    for i = 2:length(features),
         featuresreqs = strcat(featuresreqs, sprintf(',%d', features(i)));
     end
 end
